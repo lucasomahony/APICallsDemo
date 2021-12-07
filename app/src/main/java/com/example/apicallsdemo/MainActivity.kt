78252acdfb222b1e199fccb643386e97a26d57a7
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-07 11:33:39 +0000

    added permission for internet, and some helper functions for getting quotes back.

--- app/src/main/java/com/example/apicallsdemo/MainActivity.kt
+++ app/src/main/java/com/example/apicallsdemo/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.apicallsdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.apicallsdemo.databinding.ActivityMainBinding
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private fun processQuoteJson(jsonString: String): String {
+        val jsonArray = JSONArray(jsonString)
+        return jsonArray[0].toString()
+    }
+
+    fun getQuote(view: View){
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
